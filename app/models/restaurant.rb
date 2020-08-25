10a3092e2a0d3da84a8941eaf226f836df510c8b
Yusuf Ayub <yusayub@hotmail.com>
2020-08-25 13:53:25 +0200

    create map, popup and seach+autocomplete

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,6 @@ class Restaurant < ApplicationRecord
   has_many :reviews
   has_many :ingredient_reviews, through: :reviews
   has_many :ingredients, through: :ingredient_reviews
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
