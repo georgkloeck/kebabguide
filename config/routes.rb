30204b9c2c467a1b17a87d271afc09ffd66cf5a8
Doerthe Kosicki <kiki@codestun.com>
2020-08-26 16:24:36 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :cuisines, only: [:new, :show, :create]
   resources :restaurants, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:new, :create, :show]
   end
