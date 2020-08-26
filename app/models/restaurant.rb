3383e8fcda1a552a600a61441293ff50f2caf1c5
Doerthe Kosicki <kiki@codestun.com>
2020-08-26 14:32:17 +0200

    add image uploader class to restaurant class

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,9 @@
 class Restaurant < ApplicationRecord
+  mount_uploader :image, ImageUploader
   belongs_to :cuisine
   has_many :reviews
   has_many :ingredient_reviews, through: :reviews
   has_many :ingredients, through: :ingredient_reviews
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  has_one_attached :photo
 end
