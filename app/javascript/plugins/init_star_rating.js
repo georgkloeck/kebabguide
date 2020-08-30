3cbbda9ca7c6f9dc1fe457af0bbf8276d5413d75
yusuf ayub <yusayub@hotmail.com>
2020-08-30 19:22:53 +0200

    refactored code and params hash

--- app/javascript/plugins/init_star_rating.js
+++ app/javascript/plugins/init_star_rating.js
@@ -4,11 +4,20 @@ import $ from 'jquery'; // <-- if you're NOT using a Le Wagon template (cf jQuer
 const initStarRating = () => {
   $('.review_rating').barrating({
     theme: 'css-stars',
+    // theme: 'fontawesome-stars',
     onSelect:function(value, text, event)
     {
-    //  document.querySelector('#stars').innerHTML = value;
     }
 
+  });
+
+  $('.review_rating_show').barrating('show');
+
+  $('.review_rating_show').barrating({
+    // theme: 'css-stars',
+    // or
+    // theme: 'fontawesome-stars',
+    readonly: true
   });
 
 };
