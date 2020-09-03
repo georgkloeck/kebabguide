d7e557ebfbc52a7f47f6e22951cd3f8105d3a38f
Georg Kloeck <georg@kloeck.com>
2020-09-03 15:21:48 +0200

    works on local

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -44,7 +44,7 @@ class RestaurantsController < ApplicationController
   def create
     @restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
-      redirect_to @restaurant
+      redirect_to restaurant_path(@restaurant)
     else
       render :new
     end
